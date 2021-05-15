b193c01449eee9271f880ff29cd033b7dc8a1427
SLiGerr <maxbltslg@mail.ru>
2021-05-15 13:06:20 +0300

    LeanTween

---
+++ Assets/Sources/Code/IMassageContent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Sources.Code
+{
+    public interface IMassageContent
+    {
+        Vector2 RequiredRectSize { get; set; }
+        object Content { get; set; }
+    }
+}
---
+++ Assets/Sources/Code/TextContent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Sources.Code
+{
+    class TextMassageContent : IMassageContent
+    {
+        public Vector2 RequiredRectSize { get; set; }
+        public object Content { get; set; }
+
+        public TextMassageContent(string text)
+        {
+            
+            Content = text;
+        }
+    }
+}
